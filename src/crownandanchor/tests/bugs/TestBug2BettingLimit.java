60716e617d559591d8f9147df49f6c4e5f551631
Sam Stow <sam@appfactory.com.au>
2014-09-21 11:59:09 +1000

    simplified Bug 2 test case and added failing test result
    Signed-off-by: Sam Stow <sam@appfactory.com.au>

--- src/crownandanchor/tests/bugs/TestBug2BettingLimit.java
+++ src/crownandanchor/tests/bugs/TestBug2BettingLimit.java
@@ -3,8 +3,6 @@ package crownandanchor.tests.bugs;
 import static org.easymock.EasyMock.*;
 import static org.junit.Assert.*;
 
-import java.util.List;
-
 import org.easymock.EasyMock;
 import org.junit.After;
 import org.junit.Before;
@@ -22,8 +20,6 @@ public class TestBug2BettingLimit {
 	private static final int BETTING_LIMIT = 0;
 	private static final DiceValue PICK_CROWN = DiceValue.CROWN;
 	private static final int BET_AMOUNT = 5;
-	private static final int MAX_BALANCE = 300;
-	private static final int MAX_TURNS = 1500;
 	
 	private Player player;
 	private Dice d1;
@@ -72,28 +68,9 @@ public class TestBug2BettingLimit {
 		game = new Game(d1, d2, d3);
 		
 		//Play round
-		int turn = 0;
-		do {
-			System.out.printf("Turn %d: %s bet %d on %s\n",
-        			turn, player.getName(), BET_AMOUNT, PICK_CROWN); 
-        	
-        	int winnings = game.playRound(player, PICK_CROWN, BET_AMOUNT);
-            List<DiceValue> cdv = game.getDiceValues();
-            
-            System.out.printf("Rolled %s, %s, %s\n",
-            		cdv.get(0), cdv.get(1), cdv.get(2));
-            
-            if (winnings > 0) {
-                System.out.printf("%s won %d, balance now %d\n\n",
-                		player.getName(), winnings, player.getBalance());
-            }
-            else {
-                System.out.printf("%s lost, balance now %d\n\n",
-                		player.getName(), player.getBalance());
-            }
-            turn++;
-            
-		} while (player.balanceExceedsLimitBy(BET_AMOUNT) && player.getBalance() <= MAX_BALANCE && turn <= MAX_TURNS);
+		do {			
+        	game.playRound(player, PICK_CROWN, BET_AMOUNT);
+		} while (player.balanceExceedsLimitBy(BET_AMOUNT));
 		
 		verify(d1);
 		verify(d2);
